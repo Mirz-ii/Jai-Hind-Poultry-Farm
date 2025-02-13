54e8e23bac091fd44b3029cbdaaa4faac2391ac4
Mirz-ii <mirzakameel0207@gmail.com>
2025-02-13 12:45:54 +0530

    Add file

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const birdTypeSelect = document.getElementById("bird-type")
+//     const birdStatusDiv = document.getElementById("bird-status")
+  
+//     birdTypeSelect.addEventListener("change", function () {
+//       if (this.value !== "") {
+//         birdStatusDiv.classList.remove("hidden")
+//       } else {
+//         birdStatusDiv.classList.add("hidden")
+//       }
+//     })
+//   })
+  
+  
